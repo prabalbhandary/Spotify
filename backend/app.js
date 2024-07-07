546f0bd392b751a3cd60b10650fe8a581e0621d9
prabalbhandary <prabalbhandary97@gmail.com>
2024-07-07 13:31:18 +0545

    port

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 require("./db");
 require("colors")
 const morgan = require("morgan")
